5e55fa5e08f1df3c6ab66ffad3ff63c3b6bec19c
fatihselimkaraca-collab <fatihselimkaraca@gmail.com>
2026-03-29 19:50:08 +0300

    Rename supabase variable to db

--- config.js
+++ config.js
@@ -10,4 +10,4 @@ const SUPABASE_URL = 'https://jvczsajemfvishwbobzf.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2Y3pzYWplbWZ2aXNod2JvYnpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ3Nzg2NDQsImV4cCI6MjA5MDM1NDY0NH0.33pxWt6VfLcEpz0AnK8hPIKf0RoJWpK0tud3ZUaCsX4';
 
 // Initialize Supabase client
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
